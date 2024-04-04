efabcf9a298b6db5579e44ada8955884623276ba
Milan <milandahal213@gmail.com>
2024-04-03 22:49:41 -0400

    Update version.py

--- Logger+Autosave/version.py
+++ Logger+Autosave/version.py
@@ -1,1 +1,1 @@
-v={"adxl345":2,"files":2, "icons":2, "main":2, "prefs":2,  "sensors":3, "servo":2, "ssd1306":2}
+v={"adxl345":3,"files":2, "icons":2, "main":2, "prefs":2,  "sensors":3, "servo":2, "ssd1306":2}
