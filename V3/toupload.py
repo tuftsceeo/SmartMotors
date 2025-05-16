c6ac772ae8442346d6236e3908d1009315e2670e
Milan <milandahal213@gmail.com>
2025-05-16 19:18:27 -0400

    Update toupload.py

--- V3/toupload.py
+++ V3/toupload.py
@@ -1,1 +1,1 @@
-{"toupload":["adxl345.py","data.py","files.py","icons.py","main.py","prefs.py","sensors.py","servo.py","ssd1306.py","standalone.py","trainData.txt","version.py","webconnect.py",]}
+{"toupload":["adxl345.py","data.py","files.py","icons.py","main.py","prefs.py","sensors.py","servo.py","ssd1306.py","standalone.py","trainData.txt","version.py","webconnect.py"]}
